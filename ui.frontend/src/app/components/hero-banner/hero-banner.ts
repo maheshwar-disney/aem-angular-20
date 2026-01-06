f41d2d38e09a143914b8bcffe538079af6d7668a
maheshwar-disney <Maheshwar.Panshikar.-ND@disney.com>
2026-01-06 16:04:23 +0530

    Css fixes applied & hero banner using image from wdpr library

--- ui.frontend/src/app/components/hero-banner/hero-banner.ts
+++ ui.frontend/src/app/components/hero-banner/hero-banner.ts
@@ -1,15 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero-banner',
+  standalone: true,
+  imports: [CommonModule], // Add WdprMediaComponent here if it's a separate import
   templateUrl: './hero-banner.html',
-  styleUrls: ['./hero-banner.scss']
+  styleUrls: ['./hero-banner.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HeroBannerComponent {
-  // This tells Angular: "Look for the attribute 'heroimage' in the HTML"
   @Input('heroimage') heroImage: string = ''; 
-  
   @Input('alttext') altText: string = '';
-  
   @Input('heading') heading: string = '';
 }
